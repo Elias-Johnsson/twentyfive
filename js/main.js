1cd9ce2ed081c11236f1e35e507cfdbffcce78fa
Elias-johnsson <elias.johnsson5@outlook.com>
2025-03-21 14:28:49 +0100

    fixing boxes and images
    I added some transitions and styled some of the pictures.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 let slideIndex = 0;
 showSlides();
 
+let layer = document.querySelector(".movieimg");
+layer.addEventListener("click",movieTransition)
+
 function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
@@ -11,4 +14,11 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 2500);
+}
+
+function movieTransition() {
+  let layer = document.querySelector(".movie-layer");
+  layer.classList.toggle("show");
+  let layer2 = document.querySelector(".moviebox");
+  layer2.classList.toggle("up");
 }
