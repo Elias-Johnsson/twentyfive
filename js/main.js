b1bd109918717cb7c1f827301bd5a63aad6baec2
Elias-johnsson <elias.johnsson5@outlook.com>
2025-05-05 21:44:29 +0200

    finishing mobile site

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
 let slideIndex = 0;
 let animation = document.querySelector(".MOTWimage");
 animation.addEventListener("click",movieTransition)
+let animation1 = document.querySelector(".menu-button")
+animation1.addEventListener("click",navbarShow)
 let animation2 = document.querySelector(".movieimg")
 animation2.addEventListener("click",movieTransition)
 let animation3 = document.querySelector(".albumimg")
 animation3.addEventListener("click", musicTransition)
 let animation4 = document.querySelector(".albumimg2")
 animation4.addEventListener("click", musicTransition2)
-
+let animation5 = document.querySelector(".albumimg3")
+animation5.addEventListener("click", musicTransition3)
+let animation6 = document.querySelector(".albumimg4")
+animation6.addEventListener("click", musicTransition4)
 
 showSlides();
 
@@ -23,6 +28,10 @@ function showSlides() {
   setTimeout(showSlides, 2500);
 }
 
+function navbarShow(){
+  let layer = document.querySelector(".nav_bar");
+  layer.classList.toggle("show_navbar");
+}
 function movieTransition() {
   let layer = document.querySelector(".movie-layer");
   layer.classList.toggle("show");
@@ -51,4 +60,20 @@ function musicTransition2(){
   layer2.classList.toggle("show_music_divider2"); 
   let layer3 = document.querySelector(".music_review_text2");
   layer3.classList.toggle("show_music_text2");
+}
+function musicTransition3(){
+  let layer = document.querySelector(".music_reviewdiv3");
+  layer.classList.toggle("show_music_background3");
+  let layer2 = document.querySelector(".music_reviewdivider3");
+  layer2.classList.toggle("show_music_divider3"); 
+  let layer3 = document.querySelector(".music_review_text3");
+  layer3.classList.toggle("show_music_text3");
+}
+function musicTransition4(){
+  let layer = document.querySelector(".music_reviewdiv4");
+  layer.classList.toggle("show_music_background4");
+  let layer2 = document.querySelector(".music_reviewdivider4");
+  layer2.classList.toggle("show_music_divider4"); 
+  let layer3 = document.querySelector(".music_review_text4");
+  layer3.classList.toggle("show_music_text4");
 }
